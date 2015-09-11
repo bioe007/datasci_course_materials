1ea23aa8fb77c58175ae7f52165b27b8cd906a1b
Perry Hargrave <perry.hargrave@gmail.com>
2015-09-10 21:54:44 -0700

    Make twitterstream work with new API
    At some point the api used by this course was deprecated so I had to do
    some tweaking on the script.
    
    Also pulled the keys & tokens out into an un-committed 'credentials.cfg'
    file to keep these things from GH. The file is really simple, like this:
    
    [secrets]
    api_key = "secret stuff here"
    api_secret = "secret stuff here"
    access_token_key = "secret stuff here"
    access_token_secret = "secret stuff here"

--- assignment1/twitterstream.py
+++ assignment1/twitterstream.py
@@ -1,12 +1,17 @@
+import ConfigParser
 import oauth2 as oauth
 import urllib2 as urllib
 
 # See assignment1.html instructions or README for how to get these credentials
-
-api_key = "<Enter api key>"
-api_secret = "<Enter api secret>"
-access_token_key = "<Enter your access token key here>"
-access_token_secret = "<Enter your access token secret here>"
+# Parse credentials from 'don't commit' file..
+# The file format should be 'ini' style and needs each of the below named
+# variables in it.
+config = ConfigParser.ConfigParser()
+config.readfp(open('credentials.cfg'))
+api_key = config.get('secrets', 'api_key')
+api_secret = config.get('secrets', 'api_secret')
+access_token_key = config.get('secrets', 'access_token_key')
+access_token_secret = config.get('secrets', 'access_token_secret')
 
 _debug = 0
 
@@ -26,36 +31,36 @@ Construct, sign, and open a twitter request
 using the hard-coded credentials above.
 '''
 def twitterreq(url, method, parameters):
-  req = oauth.Request.from_consumer_and_token(oauth_consumer,
-                                             token=oauth_token,
-                                             http_method=http_method,
-                                             http_url=url, 
-                                             parameters=parameters)
+    req = oauth.Request.from_consumer_and_token(oauth_consumer,
+                                                token=oauth_token,
+                                                http_method=http_method,
+                                                http_url=url,
+                                                parameters=parameters)
 
-  req.sign_request(signature_method_hmac_sha1, oauth_consumer, oauth_token)
+    req.sign_request(signature_method_hmac_sha1, oauth_consumer, oauth_token)
 
-  headers = req.to_header()
+    headers = req.to_header()
 
-  if http_method == "POST":
-    encoded_post_data = req.to_postdata()
-  else:
-    encoded_post_data = None
-    url = req.to_url()
+    if http_method == "POST":
+        encoded_post_data = req.to_postdata()
+    else:
+        encoded_post_data = None
+        url = req.to_url()
 
-  opener = urllib.OpenerDirector()
-  opener.add_handler(http_handler)
-  opener.add_handler(https_handler)
+    opener = urllib.OpenerDirector()
+    opener.add_handler(http_handler)
+    opener.add_handler(https_handler)
 
-  response = opener.open(url, encoded_post_data)
+    response = opener.open(url, encoded_post_data)
 
-  return response
+    return response
 
 def fetchsamples():
-  url = "https://stream.twitter.com/1/statuses/sample.json"
-  parameters = []
-  response = twitterreq(url, "GET", parameters)
-  for line in response:
-    print line.strip()
+    url = "https://stream.twitter.com/1/statuses/sample.json"
+    parameters = []
+    response = twitterreq(url, "GET", parameters)
+    for line in response:
+        print line.strip()
 
 if __name__ == '__main__':
-  fetchsamples()
+    fetchsamples()
